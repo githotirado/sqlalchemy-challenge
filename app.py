ddbab9b5621faaf4d9459a739cffdfda624858b4
Henry Tirado <85540489+githotirado@users.noreply.github.com>
2021-09-03 21:04:56 -0700

    route changes, revision

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 ## Home Page: list all routes that are available
 @app.route("/")
 def home():
-    return """
-    Available routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
-    /api/v1.0/<start>
+    return '''
+    Available routes:</br>
+    /api/v1.0/precipitation</br>
+    /api/v1.0/stations</br>
+    /api/v1.0/tobs</br>
+    /api/v1.0/<start></br>
     /api/v1.0/<start>/<end>
-    """
+    '''
 
 ## Precipitation: return JSON of query results dictionary
 @app.route("/api/v1.0/precipitation")
@@ -27,4 +27,11 @@ def home():
 
 ## Return a JSON lisst of the min, avg, and max temperature for
 ##  a given start or start-end date range.
-@app.route("/api/v1.0/<start>" "/app/v1.0/<start>/<end>")
+# @app.route("/api/v1.0/<start>" "/app/v1.0/<start>/<end>")
+@app.route("/app/v1.0/<start>/<end>")
+def startdate(startdate, enddate):
+    return "Start and start/end"
+
+# Run if invoked by python command line
+if __name__ == "__main__":
+    app.run(debug=True)
